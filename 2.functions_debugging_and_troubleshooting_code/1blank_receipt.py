f4eee0510d42bc79de352ed4b1c2f7345c0c83c7
rtz538 <26957131+rtz538@users.noreply.github.com>
2018-02-13 00:44:09 +0200

    Homework Week 2

--- 2.functions_debugging_and_troubleshooting_code/1blank_receipt.py
+++ 2.functions_debugging_and_troubleshooting_code/1blank_receipt.py
@@ -1,14 +1,18 @@
+# EXERCISE
+# Create a function that prints a blank cash receipt. \
+# The function should invoke three other functions: one for printing the header, one for the body and one for the footer
+
+
 def print_header():
-    print('CASH RECEIPT\n' + 30 * '-')
+    print(f"CASH RECEIPT\n{30 * '-'}")
 
 
 def print_body():
-    print('Charged to' + 20 * '_')
-    print('Received by' + 19 * '_')
+    print(f"Charged to{20 * '_'}\nReceived by{19 * '_'}")
 
 
 def print_footer():
-    print(30 * '-' + f'\n\u00A9 SoftUni')
+    print(f"{30 * '-'}\n\u00A9 SoftUni")
 
 
 def print_receipt():
@@ -17,4 +21,23 @@ def print_receipt():
     print_footer()
 
 
-print(print_receipt())
+print_receipt()
+
+
+# EXAMPLES:
+#
+# CASH RECEIPT
+# ------------------------------
+# Charged to____________________
+# Received by___________________
+# ------------------------------
+# © SoftUni
+
+# HINTS:
+# 1. First create a function with no parameters for printing the header. \
+# Give it a meaningful name and write the code that it will execute.
+# 2. Do the same for printing the receipt body and footer.
+# 3. Create a function that will call all three functions in the necessary order. \
+# Again, give it a meaningful and descriptive name and write the code.
+# 4. For printing "©" use Unicode "\u00A9"
+# 5. Call (invoke) the first function
--- 2.functions_debugging_and_troubleshooting_code/2sign_of_integer_number.py
+++ 2.functions_debugging_and_troubleshooting_code/2sign_of_integer_number.py
@@ -1,3 +1,7 @@
+# EXERCISE
+# Create a function that prints the sign of an integer number n.
+
+
 def print_sign(number):
     if number > 0:
         print("The number {0} is positive.".format(number))
@@ -8,3 +12,33 @@ def print_sign(number):
 
 
 print_sign(int(input()))
+
+
+# EXAMPLES
+# Input
+# 2
+#
+# Output
+# The number 2 is positive.
+#
+#
+# Input
+# -5
+#
+# Output
+# The number -5 is negative.
+#
+#
+# Input
+# 0
+#
+# Output
+# The number 0 is zero.
+
+# HINTS
+# 1. Create a function with a descriptive name which should receive one parameter.
+# 2. Implement the body of the function by handling different cases:
+# a. If the number is greater than zero
+# b. If the number is less than zero
+# c. And if the number is equal to zero
+# 3. Call (invoke) the newly created function.
