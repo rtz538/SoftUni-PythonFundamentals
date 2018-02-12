a24132391780c7b6cd01e2037cccb2abb8dce331
rtz538 <26957131+rtz538@users.noreply.github.com>
2018-02-13 00:42:13 +0200

    Homework Week 2

---
+++ 2.functions_debugging_and_troubleshooting_code/3printing_triangle.py
@@ -0,0 +1,39 @@
+# EXERCISE
+# Create a function for printing triangles as shown in the EXAMPLES section below.
+
+
+# EXAMPLES:
+#
+# Input
+# 3
+#
+# Output
+# 1
+# 1 2
+# 1 2 3
+# 1 2
+# 1
+#
+#
+# Input
+# 4
+#
+# Output
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+
+# HINTS:
+#
+# 1. After you read the input.
+# 2. Start by creating a function for printing a single line from a given start to a given end. \
+# Choose a meaningful name for it, describing its purpose.
+# 3. Think how you can use it to solve the problem.
+# 4. After you spent some time thinking, you should have come to the conclusion that you will need two loops
+# 5. In the first loop you can print the first half of the triangle without the middle line.
+# 6. Next, print the middle line.
+# 7. Lastly, print the rest of the triangle.
